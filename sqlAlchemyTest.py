b632b2ed8ae81864265fc68795cfc0eb115dd1d2
wjseoduq2 <dragon65@naver.com>
2024-04-22 15:14:30 +0900

    final commit

--- sqlAlchemyTest.py
+++ sqlAlchemyTest.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 import pymysql
 
-data = {'학번': range(2000, 2015), '성적':[70, 60, 90, 100, 56, 63, 85, 88, 75, 99, 100,100,100,100,100]}
+data = {'학번': range(2000, 2020), '성적':[70, 60, 90, 100, 56, 63, 85, 88, 75, 99, 100,100,100,100,100, 11, 11, 11, 11, 11]}
 # 기존 테이블에 신규 데이터 추가까지 가능
 df = pd.DataFrame(data=data, columns=['학번', '성적'])
 print(df)
@@ -11,4 +11,4 @@ engine = create_engine('mysql+pymysql://guest01:12345@192.168.0.100:3306/lottodb
 engine.connect()
 
 # append는 추가, replace는 갱신, fail는 테이블이 이미 있으면 막아버림, no option도 막아버림
-df.to_sql(name='test_tbl',con=engine,if_exists='replace',index=False)
+df.to_sql(name='test_tbl',con=engine,if_exists='append',index=False)
